0b9de7688ec8eebcde24d0b3b56f277f5c77c633
Luis Augusto Trindade Curado <luisaugusto.trindadecurado@outlook.com>
2019-09-05 12:21:48 -0300

    Firt Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
-}
+  photos = [
+    {
+      url:
+      'https://thumbs.jusbr.com/filters:format(webp)/imgs.jusbr.com/publications/artigos/144922528/images/1412967253.jpeg',
+      desciption: 'Leão'
+    },
+    {
+      url:
+      'https://statig0.akamaized.net/bancodeimagens/dg/qi/e6/dgqie66ooeyri4whdebd9oqh9.jpg',
+      desciption: 'Leoa'
+    },
+    {
+      url:
+      'https://thumbs.jusbr.com/filters:format(webp)/imgs.jusbr.com/publications/artigos/144922528/images/1412967253.jpeg',
+      desciption: 'Leão'
+    }
+  ];
+  }
