6d624228923a06a3de441290f36b67d0ae09d4f0
Luis Augusto Trindade Curado <luisaugusto.trindadecurado@outlook.com>
2019-10-16 17:49:03 -0300

    Ajustes finais Parte 1

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -5,7 +5,7 @@ import { Directive, ElementRef, HostListener, Renderer, Input } from '@angular/c
 })
 export class DarkenOnHoverDirective {
 
-    @Input() brightness = '70%'
+    @Input() brightness = '70%';
     constructor(private el: ElementRef,
                 private render: Renderer) {}
 
@@ -16,7 +16,7 @@ export class DarkenOnHoverDirective {
 
     @HostListener('mouseleave')
     darkenOff() {
-        this.render.setElementStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
+        this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
     }
 
 }
